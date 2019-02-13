f07c944b585a2d4f7861d950a60f445e394a602a
pranitbhor <pranit.bhor@bonzai.co>
2019-02-13 08:18:40 +0530

    portfolioChanges

---
+++ precache-manifest.ab6ca247598eaa73fec6467cce65b42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7793016ef122bb403fb1",
+    "url": "/static/css/main.a4bb3566.chunk.css"
+  },
+  {
+    "revision": "7793016ef122bb403fb1",
+    "url": "/static/js/main.7793016e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0063c20d9ed95e8efc41cef527d047d1",
+    "url": "/index.html"
+  }
+];
